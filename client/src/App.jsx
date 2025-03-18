566b7e7529f09f57126ce7bd029050a577da6729
pjs1g162710 <pipsimpson2022@gmail.com>
2025-03-18 21:55:28 +0000

    added routing to courses page link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Login from './components/Login.jsx';
 import  Signup from './components/Signup.jsx';
+import Courses from './components/Courses.jsx';
 import { SessionProvider } from './contexts/SessionContext.jsx';
 
 
@@ -16,6 +17,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/courses" element={<Courses />} />
       </Routes>
       </SessionProvider>
     </div>
